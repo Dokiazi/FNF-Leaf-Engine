b1db7031112a7a27655d60053906fd13c927f29c
Dokiazi <cloudsbr65@gmail.com>
2025-01-31 11:14:48 -0300

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -33,7 +33,7 @@ class MainMenuState extends MusicBeatState
 	private var camAchievement:FlxCamera;
 	
 	var optionShit:Array<String> = [
-		'story_mode',
+		'storymode',
 		'freeplay',
 		'merch',
 		'options',
